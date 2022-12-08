74de782a84a068aa171f5a5a8f783cc176fbe7b2
Diego Candido <diegocan.sou@gmail.com>
2022-12-07 22:28:15 -0300

    feat: server.js

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const {Pool} = pg;
+
+const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
