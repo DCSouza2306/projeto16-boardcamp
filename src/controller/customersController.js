9be13f4cecaef472be5c5709e1d3a06dfb5d440e
Diego Candido <diegocan.sou@gmail.com>
2022-12-10 18:18:56 -0300

    feat: get customers

--- src/controller/customersController.js
+++ src/controller/customersController.js
@@ -14,3 +14,24 @@ export async function postCustomers(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getCustomers(req, res) {
+  const cpf = req.query.cpf;
+  try {
+    if (cpf) {
+      const search = `${cpf}%`;
+      const customers = await connection.query(
+        "SELECT * FROM customers WHERE cpf LIKE $1",
+        [search]
+      );
+
+      return res.send(customers.rows)
+    }
+
+    const customers = await connection.query("SELECT * FROM customers");
+    res.send(customers.rows);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+}
--- src/routes/customersRoutes.js
+++ src/routes/customersRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { bodyCustomerValidation } from "../middlewares/customersBodyValidation.middleware.js";
-import { postCustomers } from "../controller/customersController.js";
+import { postCustomers,getCustomers } from "../controller/customersController.js";
 
 const router = Router();
 
 router.post("/customers",bodyCustomerValidation, postCustomers);
 
-router.get("/customers");
+router.get("/customers", getCustomers);
 
 router.get("/customers/:id");
 
